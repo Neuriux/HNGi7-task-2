a26c8dc63f4af06efe3a7603295428e023660552
Osiegbu Precious Ebere <Docphresh91@gmail.com>
2020-06-04 19:03:23 +0100

    Update Wolfe-HNG-06360.py
    Grammatical correction and removal of whitespaces and punctuations.

--- scripts/Wolfe-HNG-06360.py
+++ scripts/Wolfe-HNG-06360.py
@@ -1,7 +1,7 @@
 #This is my python script for the stage 2 code.
 name= "Osiegbu Precious Ebere"
 id="HNG-06360"
-language="python"
+language="Python"
 email="docphresh91@gmail.com"
 
-print("Hello World, This is", name,"with HNGi7 ID", id, "and Email", email,"using", language, "for stage 2 task")
+print("Hello World, this is", name,"with HNGi7 ID", id, "and email", email,"using", language, "for stage 2 task")
